34ebb909048bacb04bed18d79807cf6d2411478e
Vidal <enclaveit@enclaveit.(none)>
2016-03-07 19:39:20 +0700

    Template and View

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,7 +4,12 @@ var ctrlMain = require('../controllers/main');
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* GET location pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* GET other page */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
